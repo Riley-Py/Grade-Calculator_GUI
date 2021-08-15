07c69a5594ca43ad71e9f038120388cc486b09bf
Riley-Py <79717436+Riley-Py@users.noreply.github.com>
2021-08-15 15:40:37 -0400

    Add files via upload

--- Grade_Calculator_GUI/Grade_Calculator_GUI.py
+++ Grade_Calculator_GUI/Grade_Calculator_GUI.py
@@ -1,12 +1,9 @@
-import tkinter as tk, threading
-from tkinter.constants import BOTH
+import tkinter as tk
 from playsound import playsound
 import statistics
 import glob
 from tkinter import font
 from PIL import Image, ImageTk
-import imageio
-
 
 liste=[]
 image_list=[]
@@ -16,23 +13,26 @@ for sound in glob.glob("SFX/*.mp3"):
     mmedmod ="SFX/3.mp3"
     twoptwo = "SFX/1.mp3"
     Yay = "SFX/4.mp3"
-    trum = "SFX/5.mp3"
+    buzz = "SFX/5.mp3"
+
 for images in glob.glob("IMG/*.*"):
     image_list.append(images)
+
 print(image_list)
+
 class window(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         img = tk.PhotoImage("photo", file="Calculator.gif")
+        self.tk.call("wm", "iconphoto", self._w, img)
         self.title("Grade Calculator")
-        self.geometry('900x600')
-        container = tk.Frame(self)
-        container.grid(row=0, column=0, sticky='nsew')
+        self.geometry('950x720')
         self.frames = {}
         self.resizable(width=False, height=False)
         self.configure(bg="gray")
-        self.tk.call("wm", "iconphoto", self._w, img)
-
+        container = tk.Frame(self)
+        container.grid(row=0, column=0, sticky='nsew')
+        
         for F in (Begin, Data, Mean, Median, Mode, Range):
             frame = F(container, self)
             self.frames[F] = frame
@@ -43,14 +43,13 @@ class window(tk.Tk):
     def showframe(self, pgname):
         frame = self.frames[pgname]
         frame.tkraise()
-
-    
-        
+   
 class Begin(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
         self.configure(bg="gray", width=1100, height=720)
+
         img = ImageTk.PhotoImage(Image.open(image_list[0]))
 
         share_tech_title = font.Font(
@@ -63,6 +62,7 @@ class Begin(tk.Frame):
         share_tech_reg = font.Font(
             family="Share Tech",
             size = 11,
+            underline=1
         )
 
         global share_tech_sub
@@ -79,12 +79,10 @@ class Begin(tk.Frame):
             underline=2
         )
 
-        
-
         lbl = tk.Label(self, text="Welcome to the Grade Calculator!",font=share_tech_title, fg="red", bg="gray")
         lbl.pack()
 
-        btn = tk.Button(self,text="Data", font= share_tech_reg,fg="blue",bg="yellow",padx=30,pady=5,width=1,command=lambda:[controller.showframe(Data), self.pop(), self.frames()])
+        btn = tk.Button(self,text="Data", font= share_tech_reg,fg="blue",bg="yellow",padx=30,pady=5,width=1,command=lambda:[controller.showframe(Data), self.pop()])
         btn.pack()
 
         btn1 = tk.Button(self,text="Mean",font= share_tech_reg,fg="blue",bg="yellow",padx=30, pady=5,width=1,command=lambda:[controller.showframe(Mean),self.pop()])
@@ -103,65 +101,62 @@ class Begin(tk.Frame):
         lblimg.image = img
         lblimg.pack(side="bottom")
 
-        
-
-
-
-
     def pop(self):
         playsound(popo, False)
 
-    
-    
-    
 
- 
-
-    
 class Data(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        global store
+        global ent
 
-        self.configure(bg="Gray")
-
-        self.lbl = tk.Label(self, text="Please enter the numbers you would like to use for the mean, median, mode, and range", font=share_tech_sub, fg="red", bg="Gray")
-        self.lbl.pack()
+        self.configure(bg="Gray", width=1100, height=720)
+        
+        lbl = tk.Label(self, text="Please enter the numbers you would like to use for the mean, median, mode, and range", font=share_tech_sub, fg="red", bg="Gray")
+        lbl.pack()
 
         self.ent = tk.Entry(self)
         self.ent.pack()
+        
 
-        self.btn = tk.Button(self, text='Submit',font=share_tech_reg, fg="blue",bg="yellow",padx=30, pady=10, width=1, command=lambda:[self.onbut(), self.yay()])
-        self.btn.pack()
+        btn = tk.Button(self, text='Submit',font=share_tech_reg, fg="blue",bg="yellow",padx=30, pady=10, width=1, command=lambda:[self.onbut(), self.yay()])
+        btn.pack()
 
-        self.btn1 = tk.Button(self, text='Back', font=share_tech_reg, fg="blue",bg="yellow",padx=30, pady=10, width=1,command=lambda:[controller.showframe(Begin), self.clear(), self.org(), self.sound()])
-        self.btn1.pack()
+        btn1 = tk.Button(self, text='Back', font=share_tech_reg, fg="blue",bg="yellow",padx=30, pady=10, width=1,command=lambda:[controller.showframe(Begin), self.clear(), self.org(), self.sound()])
+        btn1.pack()
 
     def onbut(self):
-        store = self.ent.get()
+        
         global liste
-        liste = store.split()
         global lbl
-        lbl = tk.Label(self, text="", font=share_tech_response, bg="yellow", fg="red", pady=10, padx=30)
-        try:
-            liste = [int(i) for i in liste]
-            lbl["text"] = f"Submitted!"
-            lbl.pack()
-            self.ent.delete(0, 'end')
-            print(liste)
-        except ValueError or NameError:
-            lbl["text"] = f"Enter a valid number!"
-            lbl.pack()
-            self.ent.delete(0, 'end') 
+        global lbl_image
+        store = self.ent.get()
+        liste = store.split()
+        lbl = tk.Label(self, text="Submitted!", font=share_tech_response, bg="yellow", fg="red", pady=10, padx=30)
+        lbl_image = tk.Label(self)
+
+        img = ImageTk.PhotoImage(Image.open(image_list[3]))
+        lbl_image = tk.Label(self, image=img)
+        lbl_image.image = img
+
+        liste = [int(i) for i in liste]
+        self.ent.delete(0, 'end')
+        lbl_image.pack()
+        lbl.pack()
+
+        print(liste)
+
         return liste
 
     def clear(self):
         try:
            lbl.forget()
+           lbl_image.forget()
            self.ent.delete(0, "end")
-        except NameError:
+        except AttributeError:
             pass
            
-
     def org(self):
         def partition(ar, fi, la):
             piv = ar[la]
@@ -191,33 +186,110 @@ class Mean(tk.Frame):
 
         tk.Frame.__init__(self, parent)
 
-        self.configure(bg="Gray")
+        self.configure(width=950, height=720, bg="Gray")
 
-        self.btn = tk.Button(self, text="Back", font=share_tech_reg, fg="blue",bg="yellow",padx=30, pady=10, width=1, command=lambda:[controller.showframe(Begin), self.clear(), self.back()])
-        self.btn.pack()
+        btn = tk.Button(self, text="Back", font=share_tech_reg, fg="blue",bg="yellow",padx=30, pady=10, width=1, command=lambda:[controller.showframe(Begin), self.clear(), self.back()])
+        btn.pack()
 
-        self.btn2 = tk.Button(self, text="Press me to show the mean!", bg="yellow", fg="blue", font=share_tech_reg, padx=30, pady=10, command=lambda:self.mean())
-        self.btn2.pack()
+        btn2 = tk.Button(self, text="Press me to show the mean!", bg="yellow", fg="blue", font=share_tech_reg, padx=30, pady=10, command=lambda:self.mean())
+        btn2.pack()
 
-    def mean(self):
-        global lbl
-        global lbl2
+    def mean(self):     
         try:
             c = sum(liste)/len(liste)
+
+            global lbl_image2
+            image = ImageTk.PhotoImage(Image.open(image_list[6]))
+            lbl_image2 = tk.Label(self, image=image)
+            lbl_image2.image = image
+            lbl_image2.pack()
+
+            global lbl
             lbl = tk.Label(self, text="", bg="yellow", fg="blue", font=share_tech_response, padx=30, pady=10)
             lbl["text"] = f'The mean is {c}'
             lbl.pack()
-        except NameError and ZeroDivisionError:
             playsound(twoptwo, False)
-            lbl2 = tk.Label(self, text="You must enter a list before proceeding!",font=share_tech_response, bg="yellow", fg="blue")
-            lbl2.pack()
 
+        except:
+
+            image = ImageTk.PhotoImage(Image.open(image_list[1]))
+            lbl_image2 = tk.Label(self, image=image)
+            lbl_image2.image = image
+            lbl_image2.pack()
+
+            lbl = tk.Label(self, text="",bg="yellow", fg="blue", font=share_tech_response, padx=30, pady=10)
+            lbl["text"] = f"You must enter a list before proceeding!"
+            lbl.pack()
+
+            playsound(buzz, False)
 
 
     def clear(self):
         try:
             lbl.forget()
-            lbl2.forget()
+            lbl_image2.forget()
+        except:
+            pass
+
+    def back(self):
+        playsound(mmedmod, False)
+
+class Median(tk.Frame):
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+
+        self.configure(width=950, height=720, bg="Gray")
+
+        self.btn = tk.Button(self, text="Back",bg="yellow", fg="blue", font=share_tech_reg, padx=30, pady=10, command=lambda:[controller.showframe(Begin), self.forget(), self.back()])
+        self.btn.pack()
+
+        self.btn2 = tk.Button(self, text="Click me to show the median!", bg="yellow", fg="blue", font=share_tech_reg, padx=30, pady=10, command=lambda:self.median())
+        self.btn2.pack()
+
+    def median(self):
+        global lbl1
+        lbl1 = tk.Label(self, text="",bg="yellow", fg="blue", font=share_tech_response, padx=30, pady=10)
+        image = ImageTk.PhotoImage(Image.open(image_list[5]))
+        global lbl_image
+        lbl_image = tk.Label(self, image=image)
+        lbl_image.image = image
+        try:
+            if len(liste) % 2 != 0:
+                c = liste[len(liste)//2]
+                lbl1["text"] = f"Your median is {c}."
+
+                lbl1.pack()
+                lbl_image.pack()
+
+                playsound(twoptwo, False)
+            else:
+                mid1 = liste[len(liste)//2]
+                mid2 = liste[(len(liste)//2) - 1]
+                r_mid = (mid1 + mid2) / 2
+                lbl1["text"] = f"Your median is {r_mid}."
+
+                lbl_image.pack()
+                lbl1.pack()
+
+                playsound(twoptwo, False)
+
+        except IndexError or NameError:
+            image = ImageTk.PhotoImage(Image.open(image_list[1]))
+            lbl_image = tk.Label(self, image=image)
+            lbl_image.image = image
+
+            lbl1 = tk.Label(self, text="You must enter a list before proceeding!", bg="yellow", fg="blue", font=share_tech_response, padx=30, pady=10)
+
+            lbl_image.pack()
+            lbl1.pack()
+
+            playsound(buzz, False)
+    
+    def forget(self):
+        try:
+            lbl1.forget()
+            lbl_image.forget()
         except NameError:
             pass
 
@@ -225,71 +297,54 @@ class Mean(tk.Frame):
         playsound(mmedmod, False)
 
 
-class Median(tk.Frame):
-    def __init__(self, parent, controller):
-
-        tk.Frame.__init__(self, parent)
-
-        self.btn = tk.Button(self, text="Back", command=lambda:[controller.showframe(Begin), self.forget(), self.back()])
-        self.btn.pack()
-
-        self.btn2 = tk.Button(self, text="Click me to show the median!",command=lambda:[self.median(), self.sound2()])
-        self.btn2.pack()
-
-    def median(self):
-        global lbl1
-        lbl1 = tk.Label(self, text="")
-        try:
-            if len(liste) % 2 != 0:
-                c = liste[len(liste)//2]
-                lbl1["text"] = f"Your median is {c}."
-                lbl1.pack()
-            else:
-                mid1 = liste[len(liste)//2]
-                mid2 = liste[(len(liste)//2) + 1]
-                r_mid = (mid1 + mid2) / 2
-                lbl1["text"] = f"Your median is {r_mid}."
-                lbl1.pack()
-        except IndexError:
-            lbl1 = tk.Label(self, text="You must enter a list before proceeding!")
-            lbl1.pack()
-
-    def sound2(self):
-        playsound(twoptwo, False)
-    
-    def forget(self):
-        lbl1.forget()
-
-    def back(self):
-        playsound(mmedmod, False)
-
-
-
 class Mode(tk.Frame):
     def __init__(self, parent, controller):
 
         tk.Frame.__init__(self, parent)
 
-        btn = tk.Button(self, text="Back", command=lambda:[controller.showframe(Begin), self.forgett(), self.back()])
+        self.configure(width=950, height=720, bg="Gray")
+
+        btn = tk.Button(self, text="Back",bg="yellow", fg="blue", font=share_tech_reg, padx=30, pady=10,command=lambda:[controller.showframe(Begin), self.forgett(), self.back()])
         btn.pack()
 
-        btn2 = tk.Button(self, text="Press me to show your mode!", command=lambda:self.mode())
+        btn2 = tk.Button(self, text="Press me to show your mode!",bg="yellow", fg="blue", font=share_tech_reg, padx=30, pady=10, command=lambda:self.mode())
         btn2.pack()
 
     def mode(self):
+        global lbl
+        global lbl_image
+        lbl = tk.Label(self, text="", bg="yellow", fg="blue", font=share_tech_response, padx=30, pady=10)
+        lbl_image = tk.Label(self)
         try:
+            
+
             mod = str(statistics.mode(liste))
-            global lbl
-            lbl = tk.Label(self, text="")
             lbl["text"] = f"The mode is {mod}."
-            lbl.pack()
+            
             playsound(twoptwo, False)
-        except statistics.StatisticsError:
-            lbl = tk.Label(self, text="You must enter a list before proceeding!")
+
+            img = ImageTk.PhotoImage(Image.open(image_list[2]))
+            lbl_image = tk.Label(self, image=img)
+            lbl_image.image = img
+
+            lbl_image.pack()
             lbl.pack()
+            
+        except statistics.StatisticsError:
+
+            img2 = ImageTk.PhotoImage(Image.open(image_list[1]))
+            lbl_image = tk.Label(self, image=img2)
+            lbl_image.image = img2
+            lbl_image.pack()
+
+            lbl["text"]= f"You must enter a list before proceeding!"
+            lbl.pack()
+
+            playsound(buzz, False)
 
     def forgett(self):
         lbl.forget()
+        lbl_image.forget()
     
     def back(self):
         playsound(mmedmod, False)
@@ -301,28 +356,52 @@ class Range(tk.Frame):
 
         tk.Frame.__init__(self, parent)
 
-        btn = tk.Button(self, text="Back", command=lambda:[controller.showframe(Begin), self.back_sound()])
+        self.configure(width=950, height=720, bg="gray")
+
+        btn = tk.Button(self, text="Back",bg="yellow", fg="blue", font=share_tech_reg, padx=30, pady=10, command=lambda:[controller.showframe(Begin), self.forget_sound()])
         btn.pack()
 
-        btn2 = tk.Button(self, text="Enter me to see the range!", command=lambda:[self.range(), self.sound()])
+        btn2 = tk.Button(self, text="Enter me to see the range!",bg="yellow", fg="blue", font=share_tech_reg, padx=30, pady=10,command=lambda:self.range())
         btn2.pack()
 
     def range(self):
-        ran = max(liste) - min(liste)
-        lbl = tk.Label(self, text="")
-        lbl["text"] = f"The range is {ran}."
-        lbl.pack()
-    
-    def sound(self):
-        playsound(twoptwo, False)
-    
-    def back_sound(self):
-        playsound(mmedmod, False)
+        global lbl
+        global lbl_image
+        lbl = tk.Label(self, text="",bg="yellow", fg="blue", font=share_tech_response, padx=30, pady=10)
+        lbl_image = tk.Label(self)
+        try:
 
-        
-    
+            ran = max(liste) - min(liste)
+            img = ImageTk.PhotoImage(Image.open(image_list[4]))
+            lbl_image = tk.Label(self, image=img)
+            lbl_image.image = img
 
-       
+            lbl["text"] = f"The range is {ran}."
+
+            lbl_image.pack()
+            lbl.pack()
+
+            playsound(twoptwo, False)
+        except:
+
+            img = ImageTk.PhotoImage(Image.open(image_list[1]))
+            lbl_image = tk.Label(self, image=img)
+            lbl_image.image = img
+            lbl_image.pack()
+
+            lbl["text"] = f"You must enter a list before proceeding!"
+            lbl.pack()
+
+            playsound(buzz, False)
+    
+    def forget_sound(self):
+        try:
+            lbl.forget()
+            lbl_image.forget()
+            playsound(mmedmod, False)
+        except:
+            pass
+
 if __name__ == '__main__':
     app = window()
     app.mainloop()
